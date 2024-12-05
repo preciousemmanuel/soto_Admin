79b45d6bc858652b600161bd5f786926eff09c6f
Williams Samuel <samwill300@gmail.com>
2024-12-05 18:28:29 +0100

    fixed build issue

--- src/components/shared/withdrawal-tab.tsx
+++ src/components/shared/withdrawal-tab.tsx
@@ -1,14 +1,12 @@
-import { GetSettingsQuery } from "@/queries/settings"
-import { useQuery } from "@tanstack/react-query"
 import * as React from "react"
 import { Button } from "../ui/button"
 import { Switch } from "../ui/switch"
 
 export const WithdrawalTab = () => {
-	const { data } = useQuery({
-		queryFn: GetSettingsQuery,
-		queryKey: ["get-settings"],
-	})
+	// const { data } = useQuery({
+	// 	queryFn: GetSettingsQuery,
+	// 	queryKey: ["get-settings"],
+	// })
 
 	const [field, setField] = React.useState({
 		manual: false,
